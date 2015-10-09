33910e256a8559ec4d821ff7c1ba7029355eb30d
etokrug <etokrug@gmail.com>
2015-10-09 16:09:52 -0700

    Commit Fix

--- Lessons/vowel_counter.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3
-"""Counts the vowels in a user input string."""
-
-s = input("Enter any string: ")
-vcount = 0
-for c in s:
-    if c in "aeiouAEIOU":
-        vcount += 1
-print("Vowel count:", vcount)
--- Lessons/wall_area.py
+++
@@ -1,8 +0,0 @@
-#
-# wall_are.py
-#
-h = float(input("Room height: "))
-w = float(input("Room width : "))
-d = float(input("Room depth : "))
-area = 2 * (h * (w + d))
-print("Area of walls:", area)
