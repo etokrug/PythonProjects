a34125a2cdc5e43945684cbcf2f364650079ba3d
etokrug <etokrug@gmail.com>
2015-11-23 20:54:18 -0800

    Initial model build

--- Projects/websites/FindNewNews/FindNews/models.py
+++ Projects/websites/FindNewNews/FindNews/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 
 class Project(models.Model):
+    pk_project_id = models.AutoField(primary_key=True)
     fk_user_id = models.ForeignKey(User)
     project_name = models.CharField(max_length=255)
     project_description = models.CharField(max_length=8000)
     date_created = models.DateTimeField
+
+    #Reference
+    #http://www.b-list.org/weblog/2006/nov/02/django-tips-auto-populated-fields/
+
+    # TODO: Build this model like whoa
