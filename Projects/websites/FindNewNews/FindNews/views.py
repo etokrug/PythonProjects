2d3790352d98d55b8cecf424d8d51fe8f0208040
etokrug <etokrug@gmail.com>
2015-11-30 13:38:21 -0800

    Initial View and URL templating

--- Projects/websites/FindNewNews/FindNews/views.py
+++ Projects/websites/FindNewNews/FindNews/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from .models import SearchEngines, Project
 
 
 def customsearch(request):
     return HttpResponse("Custom Search Placeholder")
 
 def createproject(request):
-    return HttpResponse("Create Project Holder")
+    search_engines = SearchEngines.objects.all()
+    context = RequestContext(request, {'search_engines': search_engines,})
+    return render(request, 'FindNews/createproject.html', context)
 
