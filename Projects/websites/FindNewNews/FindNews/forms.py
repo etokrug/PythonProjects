00a63f792df94a427ff11c85663be705965822eb
etokrug <etokrug@gmail.com>
2015-12-06 01:24:13 -0800

    New Project Form Build

--- Projects/websites/FindNewNews/FindNews/forms.py
+++ Projects/websites/FindNewNews/FindNews/forms.py
@@ -1,8 +1,17 @@
-from django import forms
-from .models import SearchEngines
+from django.forms import Form, ModelForm, Textarea, TextInput
+from .models import SearchEngines, Project
 
-class NewProjectForm(forms.Form):
+class NewProjectForm(ModelForm):
     # TODO: Include resolution to grab user information for logging!!!
-    project_name = forms.CharField(label='Project Name', max_length=255, min_length=5)
-    project_decription = forms.CharField(label='Project Description', max_length=8000)
-    project_search_engine = forms.ModelChoiceField(queryset=SearchEngines.objects.all())
+    class Meta:
+        model = Project
+        fields = ['project_name', 'project_description']
+        widgets = {
+            'project_description': Textarea(),
+        }
+    #PNAME = 'Project Name:'
+    #PDESC = 'Project Description: '
+    #PSEARCHENG = 'Project Search Engine: '
+    #project_name = forms.CharField(label='Project Name', max_length=255, min_length=5)
+    #project_decription = forms.CharField(label='Project Description', widget=forms.Textarea, max_length=8000)
+    #project_search_engine = forms.ModelChoiceField(queryset=SearchEngines.objects.all())
